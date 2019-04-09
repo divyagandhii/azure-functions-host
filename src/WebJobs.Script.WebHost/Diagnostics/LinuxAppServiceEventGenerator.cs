8df142e688603da95e931d8918d8e231232d58ab
Divya Gandhi <digandhi@microsoft.com>
2019-04-08 19:10:37 -0700

    log timestamp

--- src/WebJobs.Script.WebHost/Diagnostics/LinuxAppServiceEventGenerator.cs
+++ src/WebJobs.Script.WebHost/Diagnostics/LinuxAppServiceEventGenerator.cs
@@ -57,7 +57,8 @@ namespace Microsoft.Azure.WebJobs.Script.WebHost.Diagnostics
             string invocationId, string executionStage, long executionTimeSpan, bool success)
         {
             var logger = _loggerFactory.GetOrCreate(FunctionsExecutionEventsCategory);
-            WriteEvent(logger, $"{executionStage}");
+            string timestamp = DateTime.UtcNow.ToString(EventTimestampFormat);
+            WriteEvent(logger, $"{timestamp}");
         }
 
         private static void WriteEvent(LinuxAppServiceFileLogger logger, string evt)
